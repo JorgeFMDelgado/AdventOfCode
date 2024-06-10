1571004858d8499afbb3e36c59b300239008cae5
JorgeFMDelgado <jorgefmdelgado@gmail.com>
2024-06-09 20:02:09 -0600

    Update Day3_Part1.py

--- Day3_GearRatios/Day3_Part1.py
+++ Day3_GearRatios/Day3_Part1.py
@@ -1,32 +1,26 @@
 # Open the file with the puzzle input
 f = open("PuzzleInput.txt","r")
 
-def SplitVariousDelimiters(str,dels):
-    # Function to split a string "str" by the delimiters "dels"
-    # Input: string "str"
-    #        list "dels"
-    # Output: list "resSplit"
 
-    # For loop to add a space " " before each delimiters in "dels"
-    for d in dels:
-        str = " ".join(str.split(d))
-    
-    # The usual split function only split a string by whitespace (\t,\r,\n,\f and spaces), hence the idea of adding a space to the delimiters
-    resSplit = str.split()
+matrix = [list(line.strip()) for line in f]
 
 
-    # The function will return a list due to the split
-    return resSplit
 
-
-char = list(set(f.read()))
+char = list(set(str(matrix)))
 char.sort()
+
+
 
 NoNumericalChar = [c for c in char if not c.isdecimal()]
 
-print(NoNumericalChar)
 
-f.seek(0)
+
+for line in matrix:
+    print([n for n in line if n.isdecimal()])
+    break
+
+
+
 
 
 
